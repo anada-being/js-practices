230f8a0ff0e60807aa7aa3bffd5ea9bf485188f3
anada <anada@beingcorp.co.jp>
2024-02-28 14:04:01 +0900

    引数が正しく与えられているかを条件に関数に切り分け

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -4,45 +4,76 @@ import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2));
 const firstDate = new Date();
-
-firstDate.setFullYear(args["y"]);
-firstDate.setMonth(args["m"] - 1);
-firstDate.setDate(1);
-
-const firstDay = firstDate.getDay();
-
 const lastDate = new Date();
-lastDate.setMonth(args["m"]);
-lastDate.setDate(0);
 
-const year = firstDate.getFullYear();
+const month = args["m"];
+const year = args["y"];
 
-console.log(`   ${Intl.DateTimeFormat("en-US", { month: "long" }).format(firstDate)} ${year}`);
-console.log("Su Mo Tu We Th Fr Sa");
+try{
+  if (month != undefined && (!(month > 0 && month < 13)) ){
+    throw new Error(`cal: ${month} is not a month number (1..12)`);
+  }
+  if (year != undefined && (!(year > 1969 && year < 2101 || year === true))){
+    throw new Error(`cal: not a valid year ${year}`);
+  }
 
-for (let i = 0; i < firstDay; i++) {
-  process.stdout.write("   ");
-}
+  if(month){
+    setMonth(month);
+  }
+  if (year){
+    setYear(year);
+  }
 
-let countNumber = 1;
+  firstDate.setDate(1);
+  lastDate.setMonth(firstDate.getMonth() + 1);
+  lastDate.setDate(0);
+  const firstDay = firstDate.getDay();
 
-for (let i = firstDay; i < 7; i++) {
-  process.stdout.write(` ${countNumber}`);
-  process.stdout.write(" ");
-  countNumber++;
-}
-process.stdout.write("\n");
+  console.log(`   ${Intl.DateTimeFormat("en-US", { month: "long" }).format(firstDate)} ${firstDate.getFullYear()}`);
+  console.log("Su Mo Tu We Th Fr Sa");
 
-const saturday = (7 - firstDay) % 7;
+  for (let i = 0; i < firstDay; i++) {
+    process.stdout.write("   ");
+  }
 
-for (countNumber; countNumber <= lastDate.getDate(); countNumber++) {
-  if (countNumber < 10) {
+  let countNumber = 1;
+
+  for (let i = firstDay; i < 7; i++) {
+    process.stdout.write(` ${countNumber}`);
     process.stdout.write(" ");
+    countNumber++;
   }
-  process.stdout.write(`${countNumber}`);
-  process.stdout.write(" ");
-  if (countNumber % 7 == saturday) {
-    process.stdout.write("\n");
+  process.stdout.write("\n");
+
+  const saturday = (7 - firstDay) % 7;
+
+  for (countNumber; countNumber <= lastDate.getDate(); countNumber++) {
+    if (countNumber < 10) {
+      process.stdout.write(" ");
+    }
+    process.stdout.write(`${countNumber}`);
+    process.stdout.write(" ");
+    if (countNumber % 7 == saturday) {
+      process.stdout.write("\n");
+    }
   }
+  process.stdout.write("\n");
+} catch (e) {
+  console.log(e.message);
 }
-process.stdout.write("\n");
+
+function setMonth(month){
+  if(month == firstDate.getMonth()){
+    return true;
+  }
+  const monthForSet = args["m"] - 1;
+  firstDate.setMonth(monthForSet);
+  lastDate.setMonth(monthForSet);
+}
+function setYear(year){
+  if (year === true || year == firstDate.getFullYear()){
+    return true;
+  }
+  firstDate.setFullYear(year);
+  lastDate.setFullYear(year);
+}
