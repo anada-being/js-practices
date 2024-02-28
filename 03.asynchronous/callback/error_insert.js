fdd4c2fb3c375ede79ab4a7eb08d3c4668cc8cef
anada <anada@beingcorp.co.jp>
2024-02-28 16:03:57 +0900

    レコード追加時のエラーとレコード取得時のエラーを作成

---
+++ 03.asynchronous/callback/error_insert.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+import sqlite3 from "sqlite3";
+const db = new sqlite3.Database("./test.db");
+
+const createTableQuery = `
+  CREATE TABLE IF NOT EXISTS books (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL UNIQUE
+  )
+`;
+
+db.run("drop table if exists books", (err) => {
+  if (err) throw err;
+  db.run(createTableQuery, (err) => {
+    if (err) throw err;
+    db.run("INSERT INTO books", (err) => {
+      if (err) throw "NOT NULL";
+      db.get("select * from books where rowid = last_insert_rowid()", (err, row) => {
+        if (err) throw err;
+        console.log(row["id"]);
+        db.all("select * from books", (err, data) => {
+          if (err) throw err;
+          console.log(data);
+        });  
+      });
+    });
+  });
+});
+
+db.run("INSERT INTO books", (err) => {
+  if (err) throw "NOT NULL";
+  db.get("select * from books where rowid = last_insert_rowid()", (err, row) => {
+    if (err) throw err;
+    console.log(row["id"]);
+    db.all("select * from books", (err, data) => {
+      if (err) throw err;
+      console.log(data);
+    });  
+  });
+});
