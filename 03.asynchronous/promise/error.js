af09505b9bc155d4b4270dd1d54d65827e0d7aeb
anada <anada@beingcorp.co.jp>
2024-03-07 16:40:15 +0900

    promiseでも同様に想定していないエラーを再送出

--- 03.asynchronous/promise/error.js
+++ 03.asynchronous/promise/error.js
@@ -10,14 +10,26 @@ runPromise(
   `CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT NOT NULL UNIQUE)`,
 )
   .then(() => runPromise(db, `INSERT INTO books (title) VALUES (null)`, true))
-  .catch((err) => console.error(err.message))
+  .catch((err) => {
+    if (err["code"] == "SQLITE_CONSTRAINT") {
+      console.error(err.message);
+    } else {
+      throw err;
+    }
+  })
   .then((lastID) => {
     if (lastID) {
       console.log(lastID);
     }
     return allPromise(db, "SELECT * FROM book");
   })
-  .catch((err) => console.error(err.message))
+  .catch((err) => {
+    if (err["code"] == "SQLITE_ERROR") {
+      console.error(err.message);
+    } else {
+      throw err;
+    }
+  })
   .then((rows) => {
     if (rows) {
       console.log(rows);
