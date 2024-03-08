73ca83525e55e049fb7eb87add48606fbab243fd
anada <anada@beingcorp.co.jp>
2024-03-08 09:06:50 +0900

    クエリを1行にし、アロー関数でいいところはアロー関数にする

--- 03.asynchronous/callback/error.js
+++ 03.asynchronous/callback/error.js
@@ -5,14 +5,11 @@ import sqlite3 from "sqlite3";
 const db = new sqlite3.Database(":memory:");
 
 db.run(
-  `CREATE TABLE books (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  title TEXT NOT NULL UNIQUE
-)`,
+  `CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT NOT NULL UNIQUE)`,
   () => {
-    db.get("INSERT INTO books (title) VALUES (null)", function (err) {
+    db.run("INSERT INTO books (title) VALUES (null)", (err) => {
       console.error(err.message);
-      db.all("SELECT * FROM book", function (err) {
+      db.all("SELECT * FROM book", (err) => {
         console.error(err.message);
         db.run("DROP TABLE books");
       });
