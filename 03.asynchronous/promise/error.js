39b8c29b9cafcd8065fcb02cf5fa3541abebd07d
anada <anada@beingcorp.co.jp>
2024-03-11 09:07:44 +0900

    ファイル名の変更

--- 03.asynchronous/promise/error.js
+++ 03.asynchronous/promise/error.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import sqlite3 from "sqlite3";
-import { runPromise, allPromise } from "../common_function.js";
+import { runPromise, allPromise } from "../run_all_promise.js";
 
 const db = new sqlite3.Database(":memory:");
 
