19320534ab4273d7935986a38ab6168a56690832
anada <anada@beingcorp.co.jp>
2024-03-14 11:02:01 +0900

    バッククオートをダブルクォーテーション

--- 03.asynchronous/promise/no_error.js
+++ 03.asynchronous/promise/no_error.js
@@ -9,7 +9,7 @@ runPromise(
   db,
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
 )
-  .then(() => runPromise(db, `INSERT INTO books (title) VALUES ("Ruby入門")`))
+  .then(() => runPromise(db, "INSERT INTO books (title) VALUES ('Ruby入門')"))
   .then((result) => {
     console.log(result.lastID);
     return allPromise(db, "SELECT * FROM books");
