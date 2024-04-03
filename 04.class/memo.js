69019d8053be6b67f17a7d04a6368a1fdec54175
anada <anada@beingcorp.co.jp>
2024-04-03 17:01:57 +0900

    memoクラスだけにし、実行するファイルを分けた

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,66 +1,7 @@
-#!/usr/bin/env node
-
-import minimist from "minimist";
-import { select } from "@inquirer/prompts";
-import { MemoDB } from "./memo_db.js";
-
-async function main() {
-  const isPipedInput = !process.stdin.isTTY;
-  const argv = minimist(process.argv.slice(2));
-  const db = new MemoDB();
-
-  if (isPipedInput) {
-    process.stdin.setEncoding("utf8");
-    let inputData = "";
-    process.stdin.on("data", (data) => {
-      inputData += data;
-    });
-
-    process.stdin.on("end", async () => {
-      await db.createMemo(inputData)
-      process.exit();
-    });
-  }
-
-  if (argv.l || argv.r || argv.d) {
-    let memos = await db.getMemo();
-    memos.forEach((memo, index) => {
-      memos[index] = new Memo(memo.id, memo.content);
-    });
-    if (argv.l) {
-      memos.forEach((memo) => {
-        console.log(memo.name);
-      });
-    }
-    if (argv.r) {
-      const answer = await selectMemo(memos);
-      const found = memos.find((memo) => memo.id === answer);
-      console.log(found.content);
-    }
-    if (argv.d) {
-      const answer = await selectMemo(memos);
-      db.deleteMemo(answer);
-    }
-  }
-}
-
-async function selectMemo(memos) {
-  return await select({
-    message: "Choose a memo:",
-    choices: memos.map((memo) => ({
-      value: memo.id,
-      name: memo.title,
-      description: memo.content,
-    })),
-  });
-}
-
-class Memo {
+export class Memo {
   constructor(id, content) {
     this.id = id;
     this.title = content.slice(0, content.indexOf("\n"));
     this.content = content;
   }
 }
-
-main();
---
+++ 04.class/memo_app.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+
+import minimist from "minimist";
+import { select } from "@inquirer/prompts";
+import { MemoDB } from "./memo_db.js";
+import { Memo } from "./memo.js";
+
+async function main() {
+  const argv = minimist(process.argv.slice(2));
+  const db = new MemoDB();
+
+  process.stdin.resume();
+  process.stdin.setEncoding("utf8");
+
+  let inputData = "";
+  process.stdin.on("data", (data) => {
+    inputData += data;
+  });
+
+  process.stdin.on("end", async () => {
+    await db.createMemo(inputData)
+    process.exit();
+  });
+
+  if (argv.l || argv.r || argv.d) {
+    process.stdin.pause();
+    let memos = await db.getMemo();
+    memos.forEach((memo, index) => {
+      memos[index] = new Memo(memo.id, memo.content);
+    });
+    if (argv.l) {
+      memos.forEach((memo) => {
+        console.log(memo.title);
+      });
+    }
+    if (argv.r) {
+      const answer = await selectMemo(memos);
+      const found = memos.find((memo) => memo.id === answer);
+      console.log(found.content);
+    }
+    if (argv.d) {
+      const answer = await selectMemo(memos);
+      await db.deleteMemo(answer);
+    }
+    process.exit();
+  }
+}
+
+async function selectMemo(memos) {
+  return await select({
+    message: "Choose a memo:",
+    choices: memos.map((memo) => ({
+      value: memo.id,
+      name: memo.title,
+      description: memo.content,
+    })),
+  });
+}
+
+main();
