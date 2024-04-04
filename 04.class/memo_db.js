a9cb3371f4b68d45e0fc46df7b959cfdfea027b6
anada <anada@beingcorp.co.jp>
2024-04-04 10:58:16 +0900

    クラス外に定義してしまっていたものをクラス内になるように修正

--- 04.class/memo_db.js
+++ 04.class/memo_db.js
@@ -4,42 +4,44 @@ const db = new sqlite3.Database("./memo.db");
 
 export class MemoDB {
   async createTable() {
-    await runPromise(
+    await this.runPromise(
       db,
       "CREATE TABLE IF NOT EXISTS memos (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT NOT NULL)",
     );
   }
   async createMemo(content) {
-    await runPromise(db, "INSERT INTO memos (content) VALUES (?)", [content]);
+    await this.runPromise(db, "INSERT INTO memos (content) VALUES (?)", [
+      content,
+    ]);
   }
   async getMemo() {
-    return await allPromise(db, "SELECT * FROM memos ORDER BY id");
+    return await this.allPromise(db, "SELECT * FROM memos ORDER BY id");
   }
   async deleteMemo(id) {
-    await runPromise(db, "DELETE FROM memos WHERE id = ?", [id]);
+    await this.runPromise(db, "DELETE FROM memos WHERE id = ?", [id]);
   }
-}
 
-function runPromise(db, query, params) {
-  return new Promise((resolve, reject) => {
-    db.run(query, params, function (err) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(this);
-      }
+  runPromise(db, query, params) {
+    return new Promise((resolve, reject) => {
+      db.run(query, params, function (err) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(this);
+        }
+      });
     });
-  });
-}
+  }
 
-function allPromise(db, query) {
-  return new Promise((resolve, reject) => {
-    db.all(query, (err, rows) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(rows);
-      }
+  allPromise(db, query) {
+    return new Promise((resolve, reject) => {
+      db.all(query, (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
     });
-  });
+  }
 }
