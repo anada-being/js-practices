eb720ddd1d91eb473fef7aeb05bcf176fd79add6
anada <anada@beingcorp.co.jp>
2024-04-15 15:38:18 +0900

    命名変更、DB操作と標準入力を受け取る関数を分け、プロセスを終了せずエラーを返すように、selectMemoがMemoクラスを返すように

--- 04.class/memo_app.js
+++ 04.class/memo_app.js
@@ -5,17 +5,18 @@ import * as inquirer from "@inquirer/prompts";
 import MemoDB from "./memo_db.js";
 
 async function main() {
-  const db = await new MemoDB();
-  await db.createTable();
+  const memoDb = await new MemoDB();
+  await memoDb.createTable();
   const argv = minimist(process.argv.slice(2));
   if (Object.values(argv).length === 1) {
-    receiveStdin(db);
+    const inputData = receiveFromStdin();
+    await memoDb.createMemo(inputData);
   } else {
-    dispatchArgv(db, argv);
+    showOrDeleteMemo(memoDb, argv);
   }
 }
 
-function receiveStdin(db) {
+function receiveFromStdin() {
   process.stdin.resume();
   process.stdin.setEncoding("utf8");
 
@@ -25,45 +26,54 @@ function receiveStdin(db) {
   });
 
   process.stdin.on("end", async () => {
-    await db.createMemo(inputData);
+    return inputData;
   });
 }
 
-async function dispatchArgv(db, argv) {
-  const memos = await db.getMemos();
-  if (argv.l) {
-    memos.forEach((memo) => {
-      console.log(memo.title);
-    });
-  } else if (argv.r) {
-    const message = "choose a memo you want to see";
-    const selectedMemoID = await selectMemo(memos, message);
-    const selectedMemo = memos.find((memo) => memo.id === selectedMemoID);
-    console.log(selectedMemo.content);
-  } else if (argv.d) {
-    const message = "choose a memo you want to delete";
-    const selectedMemoID = await selectMemo(memos, message);
-    await db.deleteMemo(selectedMemoID);
+async function showOrDeleteMemo(memoDb, argv) {
+  const memos = await memoDb.getMemos();
+
+  try {
+    if (argv.l) {
+      memos.forEach((memo) => {
+        console.log(memo.title);
+      });
+    } else if (argv.r) {
+      const message = "choose a memo you want to see";
+      const selectedMemo = await selectMemo(memos, message);
+      if (selectedMemo instanceof Error) {
+        throw selectedMemo;
+      }
+      console.log(selectedMemo.content);
+    } else if (argv.d) {
+      const message = "choose a memo you want to delete";
+      const selectedMemo = await selectMemo(memos, message);
+      if (selectedMemo instanceof Error) {
+        throw selectedMemo;
+      }
+      await memoDb.deleteMemo(selectedMemo.id);
+    }
+  } catch (err) {
+    console.log(Boolean(err));
+    console.error(err.message);
   }
 }
 
 async function selectMemo(memos, message) {
-  if (memos.length === 0) {
-    console.log("メモはまだありません");
-    process.exit();
-  }
   try {
+    if (memos.length === 0) {
+      throw new Error("まだメモはありません");
+    }
     return await inquirer.select({
       message: message,
       choices: memos.map((memo) => ({
-        value: memo.id,
+        value: memo,
         name: memo.title,
         description: memo.content,
       })),
     });
   } catch (err) {
-    console.error(err.message);
-    process.exit();
+    return err;
   }
 }
 
