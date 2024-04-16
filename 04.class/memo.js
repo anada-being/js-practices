cf67bbb5a0bca3deaca0b2de5e45372a155acf9b
anada <anada@beingcorp.co.jp>
2024-04-16 19:04:01 +0900

    1行だけのメモもタイトル表示されるように

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,7 +1,13 @@
 export default class Memo {
   constructor(id, content) {
     this.id = id;
-    this.title = content.slice(0, content.indexOf("\n"));
+    this.title = content.slice(0, content.indexOf("\n")) || this.onlyRow(content);
     this.content = content;
+  }
+
+  onlyRow(content) {
+    if (content.match(/\S/) && !content.match(/\n/)) {
+      return content;
+    }
   }
 }
