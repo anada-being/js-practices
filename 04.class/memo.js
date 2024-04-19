f6f8c1a69f7fbedac439e8e9347c428d72f4cd74
anada <anada@beingcorp.co.jp>
2024-04-19 09:54:06 +0900

    titleを作成する方法をまとめた

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,17 +1,15 @@
 export default class Memo {
   constructor(id, content) {
     this.id = id;
-    this.title = this.firstRow(content) || this.onlyRow(content);
+    this.title = this.setitle(content);
     this.content = content;
   }
 
-  firstRow(content) {
-    content.slice(0, content.indexOf("\n"));
-  }
-
-  onlyRow(content) {
+  setitle(content) {
     if (content.match(/\S/) && !content.match(/\n/)) {
       return content;
+    } else {
+      return content.slice(0, content.indexOf("\n"));
     }
   }
 }
